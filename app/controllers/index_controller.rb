5ec1a20d123ae8b0aef82ed8ecaf20b9d9642df0
Ravshan <ravshansk@protonmail.com>
2017-06-20 17:56:37 +0600

    added bootstrap flatly theme

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,2 +1,4 @@
 class IndexController < ApplicationController
+  def query
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619105456) do
+ActiveRecord::Schema.define(version: 20170619184644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20170619105456) do
     t.string "course"
     t.string "dept"
     t.integer "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scrapers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
