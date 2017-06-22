02d38b4e1fc73e895380e9bd619f797294521fc5
Ravshan <ravshansk@protonmail.com>
2017-06-22 17:25:56 +0600

    added school model contents; connected form to /results page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,8 @@
 class IndexController < ApplicationController
   def query
+		@schools = School.pluck(:name)
   end
+	def results
+		render :json => params
+	end
 end
--- utilities/add_schools.rb
+++ utilities/add_schools.rb
@@ -1,7 +1,7 @@
 require 'csv'
-a = File.read("utilities/schools.csv")
+a = File.read("files/schools.csv")
 b = CSV.parse(a, headers:true)
 b.each do |be|
-	Ref.new(be.to_hash).save
+	School.new(be.to_hash).save
 end
 
